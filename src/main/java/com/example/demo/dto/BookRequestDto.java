7a2fadbcbe3bbcc5e9d4573cb75c9989d95960b6
Dan Breczinski <pt2323@gmail.com>
2026-04-17 13:32:10 +0200

    cleanup

--- src/main/java/com/example/demo/dto/BookRequestDto.java
+++ src/main/java/com/example/demo/dto/BookRequestDto.java
@@ -3,10 +3,11 @@ package com.example.demo.dto;
 import com.example.demo.enums.BookFormats;
 import lombok.Data;
 
+
 // use Lombok @Data annotation to require all args in constructor
 // create getters/setters and provide ToString method
 @Data
-public  class BookRequestDto {
+public class BookRequestDto {
     private String name;
     private String author;
     private String isbn;
