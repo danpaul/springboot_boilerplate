7d5ca86d50d7502c38fb7bdf3ea62b38899b8a31
Dan Breczinski <pt2323@gmail.com>
2026-05-08 08:28:05 +0200

    fix login with token bug

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 // Disabling CSRF protection for simplicity (not recommended for production)
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register").anonymous()
+                        .requestMatchers("/auth/login", "/auth/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
